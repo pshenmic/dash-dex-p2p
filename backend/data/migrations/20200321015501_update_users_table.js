8a2bcfb754d61a02b207417ae88b7149e02375c8
Emmanueleclipse <sunasunday111@gmail.com>
2024-04-20 20:17:15 +0500

    removed email_verification field

--- backend/data/migrations/20200321015501_update_users_table.js
+++ backend/data/migrations/20200321015501_update_users_table.js
@@ -1,15 +1,13 @@
 module.exports.up = function(knex) {
   return knex.schema.table("users", users => {
     users.string("blurb", 255);
-    users.boolean("is_email_verification").defaultTo(false);
-    users.boolean("is_merchant").defaultTo(false);
+    users.boolean("merchant").defaultTo(false);
   });
 };
 
 module.exports.down = function(knex) {
   return knex.schema.table("users", users => {
     users.dropColumn("merchant");
-    users.dropColumn("email_verification");
     users.dropColumn("blurb");
   });
 };
