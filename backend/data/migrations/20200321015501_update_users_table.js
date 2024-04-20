0ef3e0a9568e957c985d10d72ae8fa4ec5bdc48e
Emmanueleclipse <sunasunday111@gmail.com>
2024-04-20 19:47:20 +0500

    removed useless column

--- backend/data/migrations/20200321015501_update_users_table.js
+++ backend/data/migrations/20200321015501_update_users_table.js
@@ -1,10 +1,7 @@
 module.exports.up = function(knex) {
   return knex.schema.table("users", users => {
     users.string("blurb", 255);
-    users.string("country", 255);
-    users.string("city", 255);
     users.boolean("is_email_verification").defaultTo(false);
-    users.boolean("is_phone_verification").defaultTo(false);
     users.boolean("is_merchant").defaultTo(false);
   });
 };
@@ -12,10 +9,7 @@ module.exports.up = function(knex) {
 module.exports.down = function(knex) {
   return knex.schema.table("users", users => {
     users.dropColumn("merchant");
-    users.dropColumn("phone_verification");
     users.dropColumn("email_verification");
-    users.dropColumn("city");
-    users.dropColumn("country");
     users.dropColumn("blurb");
   });
 };
