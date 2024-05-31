373bb49a848a8668a26b9c04e84c7c4c87d5086e
Emmanueleclipse <sunasunday111@gmail.com>
2024-06-01 02:57:05 +0500

    indentation issue is resolved

--- backend/offers/offersController.js
+++ backend/offers/offersController.js
@@ -19,86 +19,86 @@ module.exports.getOffersByMakerId = async (req, res) => {
 
 module.exports.getAllOffers = async (req, res) => {
 
-  const allOffers = await offersModel.fetchAllOffers();
+    const allOffers = await offersModel.fetchAllOffers();
 
-  return res.status(200).json(allOffers);
+    return res.status(200).json(allOffers);
 };
 
 module.exports.getOffer = async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  if (!id) {
-    throw new InvalidInput('Please provide a valid id')
-  }
+    if (!id) {
+      throw new InvalidInput('Please provide a valid id')
+    }
 
-  const myOffer = await offersModel.findById(id);
-  return res.status(200).json(myOffer);
+    const myOffer = await offersModel.findById(id);
+    return res.status(200).json(myOffer);
 };
 
 module.exports.createOffer = async (req, res) => {
-  const newOffer = offerModel.fromJSON(req.body)
-  const checkUser = await usersModel.findById(req?.body?.makerId).then(user => user)
+    const newOffer = offerModel.fromJSON(req.body)
+    const checkUser = await usersModel.findById(req?.body?.makerId).then(user => user)
 
-  if(!checkUser) {
-    throw new NotFoundError("Maker id not found")
-  }
+    if(!checkUser) {
+      throw new NotFoundError("Maker id not found")
+    }
 
-  const newOfferId = await offersModel.saveOffer(newOffer);
+    const newOfferId = await offersModel.saveOffer(newOffer);
 
-  io.getIO().emit("offers", { action: "create", offer: newOfferId });
+    io.getIO().emit("offers", { action: "create", offer: newOfferId });
 
-  return res.status(201).json(newOfferId);
+    return res.status(201).json(newOfferId);
 };
 
 module.exports.updateOffer = async (req, res) => {
 
-  const { offerId } = req.params;
+    const { offerId } = req.params;
 
-  const updatedOffer = offerModel.fromJSON(req.body)
+    const updatedOffer = offerModel.fromJSON(req.body)
 
-  const isOfferExist = offersModel.checkOfferExistence(offerId);
+    const isOfferExist = offersModel.checkOfferExistence(offerId);
 
-  if (!isOfferExist) {
-    throw new NotFoundError("Offer or User not found!")
-  }
+    if (!isOfferExist) {
+      throw new NotFoundError("Offer or User not found!")
+    }
 
-  const updateComplete = await offersModel.updateOffer(
-    updatedOffer,
-    offerId
-  );
+    const updateComplete = await offersModel.updateOffer(
+      updatedOffer,
+      offerId
+    );
 
-  if (!updateComplete) {
-    throw new NotFoundError("Not Found!")
-  }
+    if (!updateComplete) {
+      throw new NotFoundError("Not Found!")
+    }
 
-  io.getIO().emit("offers", { action: "update", offer: updateComplete });
+    io.getIO().emit("offers", { action: "update", offer: updateComplete });
 
-  return res.status(200).json(updateComplete);
+    return res.status(200).json(updateComplete);
 };
 
 module.exports.deleteOffer = async (req, res) => {
 
-  const { offerId } = req.params;
+    const { offerId } = req.params;
 
-  const isOfferExist = await offersModel.checkOfferExistence(offerId);
+    const isOfferExist = await offersModel.checkOfferExistence(offerId);
 
-  if (isOfferExist.length === 0) {
-    throw new NotFoundError("Offer or User not found!")
-  }
-
-  const isOrderLinkedWithit = ordersModel.findOrderByOfferId(offerId)
-  isOrderLinkedWithit.then(async (orders) => {
-    if (orders?.length === 0) {
-
-      const result = await offersModel.deleteOfferById(offerId);
-
-      io.getIO().emit("offers", { action: "delete", id: result });
-
-      return res.status(200).json({ message: "offer deleted successfully!" });
-    } else {
-      return res.status(200).json({ message: "Offer can't be deleted in current state" });
+    if (isOfferExist.length === 0) {
+      throw new NotFoundError("Offer or User not found!")
     }
-  })
+
+    const isOrderLinkedWithit = ordersModel.findOrderByOfferId(offerId)
+    isOrderLinkedWithit.then(async (orders) => {
+      if (orders?.length === 0) {
+
+        const result = await offersModel.deleteOfferById(offerId);
+
+        io.getIO().emit("offers", { action: "delete", id: result });
+
+        return res.status(200).json({ message: "offer deleted successfully!" });
+      } else {
+        return res.status(200).json({ message: "Offer can't be deleted in current state" });
+      }
+    })
 
 };
 
